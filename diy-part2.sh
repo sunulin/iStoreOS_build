37aaf843c426204cd62c12a674f5bc64904c45cd
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2024-03-27 15:06:13 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,8 +3,12 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-# 删除冲突软件库，替换golang
+# 删除冲突软件库
+
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/third_party/luci-app-LingTiGameAcc
+
+# 替换golang
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
