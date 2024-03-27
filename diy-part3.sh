247b9317bf49e0f3bc0b61b5c301e0bc1eac973a
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2024-03-27 14:53:54 +0800

    Create diy-part3.sh

---
+++ diy-part3.sh
@@ -0,0 +1,4 @@
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
