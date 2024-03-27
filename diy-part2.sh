db310f7d24cd6496408932f0a87d12840ae70871
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2024-03-27 16:52:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,8 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns,trojan*}
 rm -rf feeds/third_party/luci-app-LingTiGameAcc
 
+cp -f feeds/kenzok/adguardhome feeds/packages/net/adguardhome
+cp -f feeds/kenzok/smartdns feeds/packages/net/smartdns
 # 替换golang
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
