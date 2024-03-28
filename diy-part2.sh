77ad022bf55db13e0619cd8fe67631ace88774e4
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2024-03-28 14:29:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,22 +6,13 @@
 # 删除冲突软件库
 
 # Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
 
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/third_party/luci-app-LingTiGameAcc
 
 rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns,trojan*}
-git_sparse_clone master  https://github.com/kenzok8/openwrt-packages adguardhome smartdns
-mv -f adguardhome feeds/packages/net
-mv -f smartdns feeds/packages/net
+cp -rf feeds/kenzo/adguardhome feeds/packages/net/
+cp -rf feeds/kenzo/smartdns feeds/packages/net/
 
 # 添加passwall依赖库
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages feeds/passwall-packages
