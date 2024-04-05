5ba30914dac35ee7d6f6065acbec69d2be4af03f
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2024-04-05 17:22:33 +0800

    Update diy-part2-customize.sh

--- diy-part2-customize.sh
+++ diy-part2-customize.sh
@@ -32,8 +32,8 @@ function git_sparse_clone() {
 
 # 删除重复库
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-cp -rf feeds/small/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns} feeds/packages/net/
-
+cp -rf feeds/small/{alist,mosdns,xray*,v2ray*,v2ray*,sing*} feeds/packages/net/
+cp -rf feeds/kenzo/{adguardhome,smartdns} feeds/packages/net/
 echo "
 # 科学上网-passwall
 CONFIG_PACKAGE_luci-app-passwall=y
