804c51f410c4cfc34fac7858b1c22906ee40a47a
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2024-04-06 00:15:47 +0800

    Update diy-part2-customize.sh

--- diy-part2-customize.sh
+++ diy-part2-customize.sh
@@ -53,10 +53,10 @@ rm -rf lede
 
 # 替换prometheus-node库
 # echo "替换prometheus库..."
-# mkdir openwrt-packages
-# git clone https://github.com/openwrt/packages -b master openwrt-packages
-# cp -rf openwrt-packages/utils/{prometheus-node-exporter-lua,realtek-poe} feeds/packages/utils/
-# rm -rf openwrt-packages
+# mkdir istoreos-22.03-packages
+git clone https://github.com/jjm2473/packages -b istoreos-22.03 istoreos-22.03-packages
+# cp -rf istoreos-22.03/utils/prometheus-node-exporter-lua feeds/packages/utils/prometheus-node-exporter-lua
+# rm -rf istoreos-22.03-packages
 
 echo "
 # 科学上网-passwall
