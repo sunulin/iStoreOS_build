298d591a0af3993eaeb6208df686815a4a3660fd
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2024-04-05 16:21:27 +0800

    Update diy-part2-customize.sh

--- diy-part2-customize.sh
+++ diy-part2-customize.sh
@@ -30,6 +30,9 @@ function git_sparse_clone() {
 # git_sparse_clone main https://github.com/xiaorouji/openwrt-passwall luci-app-passwall
 # git_sparse_clone main https://github.com/xiaorouji/openwrt-passwall2 luci-app-passwall2
 
+# 删除重复库
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+
 echo "
 # 科学上网-passwall
 CONFIG_PACKAGE_luci-app-passwall=y
