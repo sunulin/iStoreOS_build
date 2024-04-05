766f0036f678344e913143cd87765c67751a37ea
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2024-04-05 21:12:17 +0800

    Update diy-part2-customize.sh

--- diy-part2-customize.sh
+++ diy-part2-customize.sh
@@ -55,7 +55,7 @@ rm -rf lede
 echo "替换prometheus库..."
 mkdir openwrt-packages
 git clone https://github.com/openwrt/packages -b master openwrt-packages
-cp -rf openwrt-packages/utils/{prometheus-*} feeds/packages/utils/
+cp -rf openwrt-packages/utils/prometheus-node-exporter-lua feeds/packages/utils/prometheus-node-exporter-lua
 rm -rf openwrt-packages
 
 echo "
