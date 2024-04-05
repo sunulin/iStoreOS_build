8435c12371db866efe3ade7b89da75d11319281f
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2024-04-06 00:36:00 +0800

    Update diy-part2-customize.sh

--- diy-part2-customize.sh
+++ diy-part2-customize.sh
@@ -52,11 +52,11 @@ cp -rf lede/libs/{glib2,pcre2} feeds/packages/libs/
 rm -rf lede
 
 # 替换prometheus-node库
-# echo "替换prometheus库..."
-# mkdir istoreos-22.03-packages
+echo "替换prometheus库..."
+mkdir istoreos-22.03-packages
 git clone https://github.com/jjm2473/packages -b istoreos-22.03 istoreos-22.03-packages
-# cp -rf istoreos-22.03/utils/prometheus-node-exporter-lua feeds/packages/utils/prometheus-node-exporter-lua
-# rm -rf istoreos-22.03-packages
+cp -rf istoreos-22.03/utils/prometheus-node-exporter-lua feeds/packages/utils/prometheus-node-exporter-lua
+rm -rf istoreos-22.03-packages
 
 echo "
 # 科学上网-passwall
