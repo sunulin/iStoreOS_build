09d9e5828500d86820d976327eb689da9cc1c267
qazz_shyper <95179414+qazz-shyper@users.noreply.github.com>
2024-04-05 12:32:15 +0800

    Update diy-part2-customize.sh

--- diy-part2-customize.sh
+++ diy-part2-customize.sh
@@ -1,1 +1,98 @@
 
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# 修改openwrt登陆地址,把下面的 10.0.0.1 修改成你想要的就可以了
+sed -i 's/192.168.1.1/10.0.0.201/g' package/base-files/files/bin/config_generate
+
+# 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/bin/config_generate
+
+# 移除要替换的包
+rm -rf feeds/packages/net/mosdns
+#rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/third_party/luci-app-LingTiGameAcc
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# 添加额外插件
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-adguardhome
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-openclash
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-aliddns
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-filebrowser filebrowser
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-jellyfin luci-lib-taskd
+
+# 加入OpenClash核心
+chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
+$GITHUB_WORKSPACE/preset-clash-core.sh
+
+# 更改 Argon 主题背景
+cp -f $GITHUB_WORKSPACE/argon/img/bg1.jpg feeds/third/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp -f $GITHUB_WORKSPACE/argon/img/argon.svg feeds/third/luci-theme-argon/htdocs/luci-static/argon/img/argon.svg
+cp -f $GITHUB_WORKSPACE/argon/background/background.jpg feeds/third/luci-theme-argon/htdocs/luci-static/argon/background/background.jpg
+cp -f $GITHUB_WORKSPACE/argon/favicon.ico feeds/third/luci-theme-argon/htdocs/luci-static/argon/favicon.ico
+cp -f $GITHUB_WORKSPACE/argon/icon/android-icon-192x192.png feeds/third/luci-theme-argon/htdocs/luci-static/argon/icon/android-icon-192x192.png
+cp -f $GITHUB_WORKSPACE/argon/icon/apple-icon-144x144.png feeds/third/luci-theme-argon/htdocs/luci-static/argon/icon/apple-icon-144x144.png
+cp -f $GITHUB_WORKSPACE/argon/icon/apple-icon-60x60.png feeds/third/luci-theme-argon/htdocs/luci-static/argon/icon/apple-icon-60x60.png
+cp -f $GITHUB_WORKSPACE/argon/icon/apple-icon-72x72.png feeds/third/luci-theme-argon/htdocs/luci-static/argon/icon/apple-icon-72x72.png
+cp -f $GITHUB_WORKSPACE/argon/icon/favicon-16x16.png feeds/third/luci-theme-argon/htdocs/luci-static/argon/icon/favicon-16x16.png
+cp -f $GITHUB_WORKSPACE/argon/icon/favicon-32x32.png feeds/third/luci-theme-argon/htdocs/luci-static/argon/icon/favicon-32x32.png
+cp -f $GITHUB_WORKSPACE/argon/icon/favicon-96x96.png feeds/third/luci-theme-argon/htdocs/luci-static/argon/icon/favicon-96x96.png
+cp -f $GITHUB_WORKSPACE/argon/icon/ms-icon-144x144.png feeds/third/luci-theme-argon/htdocs/luci-static/argon/icon/ms-icon-144x144.png
+
+echo "
+# 额外组件
+CONFIG_GRUB_IMAGES=y
+CONFIG_VMDK_IMAGES=y
+
+# openclash
+CONFIG_PACKAGE_luci-app-openclash=y
+
+# adguardhome
+CONFIG_PACKAGE_luci-app-adguardhome=y
+
+# mosdns
+CONFIG_PACKAGE_luci-app-mosdns=y
+
+# pushbot
+CONFIG_PACKAGE_luci-app-pushbot=y
+
+# Jellyfin
+CONFIG_PACKAGE_luci-app-jellyfin=y
+
+# qbittorrent
+CONFIG_PACKAGE_luci-app-qbittorrent=y
+
+# transmission
+CONFIG_PACKAGE_luci-app-transmission=y
+CONFIG_PACKAGE_transmission-web-control=y
+
+# uhttpd
+#CONFIG_PACKAGE_luci-app-uhttpd=y
+
+# 阿里DDNS
+CONFIG_PACKAGE_luci-app-aliddns=y
+
+# filebrowser
+CONFIG_PACKAGE_luci-app-filebrowser=y
+
+" >> .config
